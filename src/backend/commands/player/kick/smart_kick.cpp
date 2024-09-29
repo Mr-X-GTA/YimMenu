9e9b0f8c7ea6a836692dc419704923314eb8db1c
CringeArab <evmeschenco@gmail.com>
2024-09-29 11:49:11 +0300

    removed desync kick

--- src/backend/commands/player/kick/smart_kick.cpp
+++ src/backend/commands/player/kick/smart_kick.cpp
@@ -19,10 +19,7 @@ namespace big
 			}
 			else
 			{
-				if (player->is_host())
-					player_command::get("battleupdate"_J)->call(player, {});
-				else
-					player_command::get("desync"_J)->call(player, {});
+				player_command::get("battleupdate"_J)->call(player, {});
 			}
 		}
 	};
